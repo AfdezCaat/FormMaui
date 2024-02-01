f5ee47352e66733512d092f684535dc9b4a2d28e
AfdezCaat <arantzazufdezcast@gmail.com>
2024-02-02 00:01:09 +0100

    Agregar archivos de proyecto.

---
+++ FormMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FormMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FormMaui/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace FormMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+          /*  count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);*/
+        }
+
+      
+
+        private void ModificarBtn_Clicked(object sender, EventArgs e)
+        {
+            textNombre.Placeholder = "Nuevo Placeholder";
+            labelNombre.Text = "¡Botón Enviado!";
+        }
+        private void cambiarTextoLabel(object sender, EventArgs e)
+        {
+            labelDireccion.Text = textDireccion.Text;
+        }
+    }
+
+}
